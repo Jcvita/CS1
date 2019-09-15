c3f81e2fbea7756c1b1eae5099fa15074e5e4123
Joe Vita <jcvita2012@gmail.com>
2019-09-15 17:10:22 -0400

    update

---
+++ Week3/triangles.py
@@ -0,0 +1,41 @@
+import turtle
+
+
+def main():
+    """
+        Main function sets up the turtle and then calls make_triangle
+    """
+    depth = int(input("How many layers of depth do you want?"))
+    turtle.left(120)
+    make_triangle(300, depth)
+    turtle.left(150)
+    turtle.done()
+
+
+def make_triangle(size, layers):
+    """
+        param: size
+        param: layers
+
+        precondition: pen is down
+        precondition: turtle is facing at a 120 degree angle
+        postcondition: turtle is facing at a 120 degree angle
+        postcondition: pen is down
+    """
+
+    if layers <= 0:
+        return 0
+    else:
+        layers = layers - 1
+        turtle.forward(size)
+        make_triangle(size // 2, layers)
+        turtle.right(120)
+        turtle.forward(size)
+        turtle.left(120)
+        make_triangle(size // 2, layers)
+        turtle.right(240)
+        turtle.forward(size)
+        turtle.right(120)
+
+
+main()
